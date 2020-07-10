524162760448b14356cfbb47f415191d7cca1556
Christopher Hertel <mail@christopher-hertel.de>
2020-07-10 17:03:46 +0200

    extend plugin classes to support composer plugin api v2

--- src/Composer/CustomDirectoryInstaller/PearPlugin.php
+++ src/Composer/CustomDirectoryInstaller/PearPlugin.php
@@ -5,13 +5,23 @@ namespace Composer\CustomDirectoryInstaller;
 use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
-use Composer\CustomDirectoryInstaller\PearInstaller;
 
 class PearPlugin implements PluginInterface
 {
   public function activate (Composer $composer, IOInterface $io)
   {
+    if (!class_exists('Composer\Composer\Installer\PearInstaller')) {
+      return;
+    }
     $installer = new PearInstaller($io, $composer);
     $composer->getInstallationManager()->addInstaller($installer);
+  }
+
+  public function deactivate(Composer $composer, IOInterface $io)
+  {
+  }
+
+  public function uninstall(Composer $composer, IOInterface $io)
+  {
   }
 }
